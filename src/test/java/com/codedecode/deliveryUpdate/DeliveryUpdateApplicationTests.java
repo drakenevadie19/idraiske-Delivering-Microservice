2ab6ee74ec0169e02de7cd10522764a26a7c9313
drakenevadie19 <drakennievadje19@gmail.com>
2025-05-03 16:35:38 -0500

    New change

---
+++ src/test/java/com/codedecode/deliveryUpdate/DeliveryUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedecode.deliveryUpdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
